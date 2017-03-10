5d791d9a450335e737f6e80beeb0d8c582fa1bc8
alvaroc20 <vivamatlab@gmail.com>
2017-03-11 00:11:23 +0100

    Cambios sin guardar anterior commit

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -24,10 +24,14 @@ public class VentanaPrincipal extends JFrame{
 		getContentPane().setLayout(null);
 		
 		btnVerUsuarios = new JButton("Ver usuarios");
+		btnVerUsuarios.setForeground(new Color(0, 0, 0));
+		btnVerUsuarios.setBackground(new Color(139, 0, 139));
 		btnVerUsuarios.setBounds(149, 120, 126, 38);
 		getContentPane().add(btnVerUsuarios);
 		
 		btnVerDesp = new JButton("Mi despensa");
+		btnVerDesp.setForeground(Color.BLACK);
+		btnVerDesp.setBackground(new Color(139, 0, 139));
 		btnVerDesp.setBounds(149, 180, 126, 38);
 		getContentPane().add(btnVerDesp);
 		
@@ -46,12 +50,24 @@ public class VentanaPrincipal extends JFrame{
 		getContentPane().add(label_1);
 		
 		JButton btnNuevoUsuario = new JButton("Nuevo Usuario");
+		btnNuevoUsuario.setForeground(new Color(0, 0, 0));
+		btnNuevoUsuario.setBackground(new Color(139, 0, 139));
 		btnNuevoUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
 		btnNuevoUsuario.setBounds(149, 64, 126, 38);
 		getContentPane().add(btnNuevoUsuario);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Alvaro\\Downloads\\add-user-64x64_converted.png"));
+		lblNewLabel.setBounds(100, 64, 44, 43);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon("F:\\FamiliaDPM.png"));
+		label_2.setBounds(100, 120, 32, 38);
+		getContentPane().add(label_2);
 		
 	}
 }
