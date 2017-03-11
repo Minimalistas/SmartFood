d45124bd1601e72d6dcc068d70cb844ff67423ae
Jaime <Jaime.FernandezBravo1@alu.uclm.es>
2017-03-11 13:13:09 +0100

    añadidos listener botones label y boton usuario

--- src/GUI/Usuarios.java
+++ src/GUI/Usuarios.java
@@ -6,13 +6,14 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
+
+import com.sun.xml.internal.ws.api.server.Container;
 
 import packageDefault.ClaseMainSF;
 import packageDefault.Usuario;
@@ -53,6 +54,25 @@ public class Usuarios extends JFrame  {
 			((JLabel) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).setBounds(10, 10+87*i, 69, 62);
 			((JLabel) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).setVisible(true);
 			((JLabel) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).setEnabled(true);
+			((JLabel) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).addMouseListener(new MouseListener() {
+				public void mouseClicked(MouseEvent e) {
+					VentanaNuevoUsuario ventMostrarUsuario = new VentanaNuevoUsuario(false);
+					ventMostrarUsuario.recibirDatos(listaUsuarios.get((obtenerIndiceControl(((JLabel)e.getSource())))/2));
+				}
+				public void mouseEntered(MouseEvent e) {
+					
+				}
+				public void mouseExited(MouseEvent e) {
+					
+				}
+				public void mousePressed(MouseEvent e) {
+					
+				}
+				public void mouseReleased(MouseEvent e) {
+					
+				}
+				
+			});
 			scrllPaneUsuarios.add(new JButton(listaUsuarios.get(i).getNombre()));
 			((JButton) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).setBounds(118, 32+87*i, 86, 24);
 			((JButton) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).setVisible(true);
@@ -60,10 +80,26 @@ public class Usuarios extends JFrame  {
 			((JButton) scrllPaneUsuarios.getComponent(scrllPaneUsuarios.getComponentCount()-1)).addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					VentanaNuevoUsuario ventMostrarUsuario = new VentanaNuevoUsuario(false);
-					
+					ventMostrarUsuario.recibirDatos(listaUsuarios.get((obtenerIndiceControl(((JButton)e.getSource()))-1)/2));
 				}
 				
 			});
 		}
 	}
+	
+	@SuppressWarnings("null")
+	private int obtenerIndiceControl(JComponent comp) {
+		
+		int indice;
+		
+		for (int i=0; i<scrllPaneUsuarios.getComponentCount(); i++) {
+			if (scrllPaneUsuarios.getComponent(i).equals(comp)) {
+				return i;
+			}
+		}
+		
+		return (Integer) null;
+		
+	}
+	
 }
